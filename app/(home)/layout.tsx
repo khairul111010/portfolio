0ebbd64edfa16b085eac360c5c68277ed1421e3b
Khairul Hasan <khairul18103199@gmail.com>
2024-12-13 14:20:43 +0600

    portfolio commit

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Frontend Developer | React.js Portfolio - Khairul Hasan",
+  description:
+    "Explore the portfolio of Khairul Hasan, a skilled frontend developer specializing in React.js. View projects, skills, and expertise in modern web development.",
+  icons: {
+    icon: "/favicon.svg",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
