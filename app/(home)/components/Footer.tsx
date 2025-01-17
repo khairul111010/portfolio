0cbaeb7fcd357cddb08403e19f7d6fa409b34d3c
Khairul Hasan <khairul18103199@gmail.com>
2025-01-18 03:41:54 +0600

    footer

--- app/(home)/components/Footer.tsx
+++ app/(home)/components/Footer.tsx
@@ -1,5 +1,22 @@
+import { JetBrains_Mono } from "next/font/google";
+const jetBrains = JetBrains_Mono({
+  variable: "--font-jetbrains",
+  weight: "300",
+  subsets: ["latin"],
+});
 const Footer = () => {
-  return <footer className="h-screen">Footer</footer>;
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer
+      className={`h-20 ${jetBrains.className} text-[10px] leading-none border-t border-[#303030] uppercase text-[#a1a1a1] bg-[#101010] py-5 flex items-center justify-center`}
+    >
+      <div className="max-w-[1240px] px-5 flex items-center justify-between">
+        <div>© Khairul Hasan {currentYear}. All rights reserved.</div>
+        <div></div>
+        <div></div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,23 +1,22 @@
 "use client";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Footer from "./components/Footer";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
   return (
     <main className="bg-black text-white">
-      <section className="h-screen flex items-center justify-center border-b border-red-600">
+      <section className="h-screen flex items-center justify-center">
         <h1 className="text-6xl font-bold">Scroll Down</h1>
       </section>
 
-      <section className="h-screen flex items-center justify-center border-b border-red-600">
+      <section className="h-screen flex items-center justify-center">
         <h2 className="text-4xl">Fade Up Animation</h2>
       </section>
 
-      <section className="h-screen flex items-center justify-center">
-        <h2 className="text-4xl">Scale Animation with Scrub</h2>
-      </section>
+      <Footer />
     </main>
   );
 }
